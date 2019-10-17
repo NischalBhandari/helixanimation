deb2d4416f0c0ff15defc23406ffe86ebfc7afeb
nischal <niscalbhandari12@gmail.com>
2019-10-17 22:41:56 +0545

    small screen

--- js/game.js
+++ js/game.js
@@ -11,15 +11,15 @@ function MainLoop(){
 	this.FRAME_RATE=1000/this.FPS;
 	this.array=[];
 	this.secondArray=[];
-	for(var i=0;i<20;i++){
-		for(var j=0;j<5;j++){
-				var ballObject=new ball(this.canvas,this.ctx,i,(j+5)*20+Math.sin(i*(1-0.8))*50,'green').init();
+	for(var i=0;i<10;i++){
+		for(var j=0;j<8;j++){
+				var ballObject=new ball(this.canvas,this.ctx,i,(j+5)*20+Math.sin(i*(0.8)+180)*50,'green').init();
 	this.array.push(ballObject);
 		}
 
 	}
-	for(var i=0;i<20;i++){
-		for(var j=0;j<5;j++){
+	for(var i=0;i<10;i++){
+		for(var j=0;j<8;j++){
 				var newObject=new ball(this.canvas,this.ctx,i,(j+5)*20+Math.sin(i*0.8)*50,'green').init();
 	this.secondArray.push(newObject);
 		}
@@ -27,13 +27,13 @@ function MainLoop(){
 	}
 	setInterval(function(){
 		that.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-		for(var j=0;j<100;j++){	
+		for(var j=0;j<80;j++){	
 
 		that.array[j].move(true);
 		that.array[j].init();
 		that.array[j].collide();
 		}
-	for(var j=0;j<100;j++){	
+	for(var j=0;j<80;j++){	
 
 		that.secondArray[j].move(false);
 		that.secondArray[j].init();
